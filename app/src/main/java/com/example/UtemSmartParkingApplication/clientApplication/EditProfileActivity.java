2fd173152ab4cdc3061d4e87460a0f32ca23c86f
Angel <qifang_2012@outlook.com>
2021-12-30 18:56:54 +0800

    Add specific scan for ESP 32 devices in bluetooth(need to test) 30/12/2021

---
+++ app/src/main/java/com/example/UtemSmartParkingApplication/clientApplication/EditProfileActivity.java
@@ -0,0 +1,17 @@
+package com.example.UtemSmartParkingApplication.clientApplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.UtemSmartParkingApplication.R;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState1) {
+        super.onCreate(savedInstanceState1);
+        setContentView(R.layout.edit_profile);
+    }
+}
